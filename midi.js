886ea290532640be32e9e4e210ffe62a964923a9
MarquisdeGeek <stevengoodwin+mdg@gmail.com>
2024-07-14 21:59:41 +0100

    feat: Add MIDI constants (#26)
    Co-authored-by: Steven Goodwin <marquisdegeek@gmail.com>

--- midi.js
+++ midi.js
@@ -7,6 +7,17 @@ const Stream = require('stream');
 // MIDI input inherits from EventEmitter
 const { EventEmitter } = require('events');
 
+// Bring in the set of constants to reflect MIDI messages and their
+// parameters, to eliminate the need for magic numbers.
+/** An instrument list, only valid in the General MIDI standard */
+const Instruments = require('./instruments');
+/** A drum map, only valid in the General MIDI standard */
+const Drums = require('./drums');
+/** Note descriptions, with Middle C = C5 = MIDI note 60 */
+const Notes = require('./notes');
+/** Message names, including CCs */
+const Messages = require('./messages');
+
 class Input extends EventEmitter {
   constructor() {
     super()
@@ -153,6 +164,13 @@ module.exports = {
 
   createReadStream,
   createWriteStream,
+
+  Constants: {
+    Instruments,
+    Drums,
+    Notes,
+    Messages,
+  },
   
   // Backwards compatibility.
   input: Input,
